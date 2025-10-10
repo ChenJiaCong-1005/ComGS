2f0e9dea5bbb5d1099cec82c81fa3a9b35e46867
NIC-CJC <135231302+ChenJiaCong-1005@users.noreply.github.com>
2025-10-10 17:00:53 +0800

    Update Title.vue

--- src/components/sections/Title.vue
+++ src/components/sections/Title.vue
@@ -162,7 +162,8 @@ const buttons = [
 	<el-row justify="center">
 	  <el-button
 	    class="title-button"
-	    ：color="btn_color"
+		:color="btn_color"
+	    type="primary"
 	    text
 	    v-for="address in addresses"
 	    :key="address.name"
