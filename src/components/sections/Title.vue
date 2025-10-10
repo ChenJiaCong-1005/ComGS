e7ce1b947a3551ae67cc5bdf39a92ca964a3bb82
NIC-CJC <135231302+ChenJiaCong-1005@users.noreply.github.com>
2025-10-10 18:24:30 +0800

    Update Title.vue

--- src/components/sections/Title.vue
+++ src/components/sections/Title.vue
@@ -122,7 +122,7 @@ const buttons = [
     <!-- 最新消息提示 -->
     <el-row justify="center">
       <el-col :span="24">
-        <el-alert title=news />
+        <el-alert title="🔥 [2025-09-19] This paper is accepted by NeurIPS 2025." />
       </el-col>
     </el-row>
 
