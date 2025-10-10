fe677a3a1ea195b28db4cef3c5c897cb841892b8
NIC-CJC <135231302+ChenJiaCong-1005@users.noreply.github.com>
2025-10-10 18:19:10 +0800

    Update Header.vue

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -6,29 +6,21 @@ import { HomeFilled, SuccessFilled } from '@element-plus/icons-vue'
 // 个人主页
 const home = {
     name: "My Blog",
-    link: "https://junyaohu.github.io/",
+    link: "https://github.com/ChenJiaCong-1005/",
 }
 
 // 项目主页
 const logo = {
-    name: "Template",
+    name: "ComGS",
     link: "#",
 }
 
 // 右侧更多菜单
 const more_paper = {
-    "ExtDM": {
-        name: "ExtDM - CVPR 2024",
-        link: "https://zzcheng.top/ExtDM/",
-    },
-    "MPOT": {
-        name: "MPOT - ICCV 2023",
-        link: "https://zzcheng.top/MPOT/",
-    },
-    "ControlNet": {
-        name: "ControlNet - ICCV 2023",
-        link: "https://github.com/lllyasviel/ControlNet",
-    }
+    //"ExtDM": {
+    //    name: "ExtDM - CVPR 2024",
+    //    link: "https://zzcheng.top/ExtDM/",
+    //},
 }
 
 </script>
